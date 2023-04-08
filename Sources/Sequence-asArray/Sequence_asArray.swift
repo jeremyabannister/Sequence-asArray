c1da5ef00a6a057f0376410517dcf82f16fc0b6e
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-04-09 01:52:18 +0200

    Added `Sequence.asArray()`.

--- Sources/Sequence-asArray/Sequence_asArray.swift
+++ Sources/Sequence-asArray/Sequence_asArray.swift
@@ -1,6 +1,15 @@
-public struct Sequence_asArray {
-    public private(set) var text = "Hello, World!"
+//
+//  Sequence_asArray.swift
+//  
+//
+//  Created by Jeremy Bannister on 4/9/23.
+//
 
-    public init() {
+///
+extension Sequence {
+    
+    /// This simply calls `self.map { $0 }` to forcibly convert this value to an `Array`.
+    public func asArray () -> Array<Element> {
+        self.map { $0 }
     }
 }
---
+++ Tests/Sequence-asArray-tests/Sequence_asArray_tests.swift
@@ -0,0 +1,32 @@
+//
+//  Sequence_asArray_tests.swift
+//  
+//
+//  Created by Jeremy Bannister on 4/9/23.
+//
+
+///
+import Sequence_asArray
+import XCTest
+
+
+///
+final class Sequence_asArray_tests: XCTestCase {
+    
+    ///
+    func test_asArray () throws {
+        
+        ///
+        let array: [Int] = [4, 6, 71, 4, 12, 7, 9, 2]
+        
+        ///
+        func echoArray (_ array: [Int]) -> [Int] { array }
+        
+        ///
+        XCTAssertEqual(
+//            echoArray(array[2...5]) // Demonstration that .asArray() is necessary
+            echoArray(array[2...5].asArray()),
+            [71, 4, 12, 7]
+        )
+    }
+}
